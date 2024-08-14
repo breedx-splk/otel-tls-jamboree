07ab142e091b2921d21d2e8af6d07da6bd237f40
Jason Plumb <jplumb@splunk.com>
2024-08-14 11:06:08 -0700

    setup

---
+++ collector.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -f env.sh ] ; then
+    source env.sh
+else
+    echo "You must provide an env.sh file that contains SPLUNK_ACCESS_TOKEN".
+    exit 1
+fi
+
+if [ "" == "${SPLUNK_ACCESS_TOKEN}" ] ; then
+  echo "You must define SPLUNK_ACCESS_TOKEN env var."
+  exit 1
+fi
+
+./otelcol_darwin_arm64 --config collector.yaml \
