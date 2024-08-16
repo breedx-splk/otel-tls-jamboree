3104392592c1c62dbcc09549ae06ed215d6d72f9
Jason Plumb <jplumb@splunk.com>
2024-08-16 13:09:24 -0700

    update classpath

--- petclinic.sh
+++ petclinic.sh
@@ -3,8 +3,17 @@
 export OTEL_RESOURCE_ATTRIBUTES=deployment.environment=tls-jamboree
 
 export JAVA_HOME=$(pwd)/amazon-corretto-17.jdk/Contents/Home
+for f in lib/bc*.jar ; do
+  if [ "$CP" != "" ] ; then
+    CP="${CP}:"
+  fi
+  CP=${CP}$f
+done
+export CP
+echo $CP
 
 ${JAVA_HOME}/bin/java \
+  -cp "${CP}" \
   -Xmx512m -javaagent:./lib/splunk-otel-javaagent-2.6.0.jar \
   -Dotel.exporter.otlp.endpoint=https://localhost:4318 \
   -Dotel.javaagent.debug=true \
